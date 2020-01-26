36a3e62a3d42091c761f982497b3986ee9aa17bf
muli3203 <muli3203@gmail.com>
2020-01-26 13:09:11 +0300

    Added docstrings that explain more about the class initiated

--- user.py
+++ user.py
@@ -4,8 +4,19 @@ class User:
         """
 
 
-    def __init__(self, username, passworrd):
+    def __init__(self, username, password):
         self.username = username
         self.password = password
+
+        """
+        __init__ method help us define properties for the objects 
+        
+        Arguments:
+
+        username: new username
+        password: new password
+
+        self variable represents the instance of the object it self
+        """
 
     pass

5b7bcc03cee6f9999a33bfee752028194ffccc99
muli3203 <muli3203@gmail.com>
2020-01-26 22:01:03 +0300

    Add a class test test credential and follow it up with a setup method that runs before each testcase

--- credential_test.py
+++ credential_test.py
@@ -1,3 +1,13 @@
+import unittest
 from credentials import Credential
 from user import User
 
+class TestCredential(unittest.TestCase):
+    """
+    this is a test class that defines the test cases for a user class
+    """
+    def setUp(self):
+        """
+        A setUp method runs before each testcase
+        """
+        self.new_credentials = Credential("Cheryl", "twitter", "cher1855")

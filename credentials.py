4d78728bae8bca83a2e49bd1b8f3a5f08fb3563e
muli3203 <muli3203@gmail.com>
2020-01-26 21:37:08 +0300

    created a docstring for the init method and definied  a save credentials fucntion that saves user objects

--- credentials.py
+++ credentials.py
@@ -9,3 +9,12 @@ def __init__(self,username,password, appname):
     self.password
     self.appname
 
+    """
+    __init__ method help us define properties for the objects 
+    """
+
+def save_credential(self):
+    """
+    the save credential method saves the user objects into the credential list
+    """
+    Credential.credential_list.append(self)

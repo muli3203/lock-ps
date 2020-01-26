7dd68f3d7e4c34b75ca7f02b371619df0ff5974b
muli3203 <muli3203@gmail.com>
2020-01-26 14:21:11 +0300

    created a test init test case to test for properly initialized objects

--- user_test.py
+++ user_test.py
@@ -16,3 +16,14 @@ class TestUser(unittest.TestCase):
         Set up method to run after each test case
         """
         self.new_user = User("Cheryl", "cher1855")
+
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized property
+        """
+        self.assertEqual(self.new_user.username,"Cheryl")
+        self.assertEqual(self.new_user.password,"cher1855")
+
+
+if __name__ == '__main__':
+    unittest.main()

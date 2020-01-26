75238f4c9ee777d06c71bd4e20a2d131141e3b38
muli3203 <muli3203@gmail.com>
2020-01-26 13:40:59 +0300

    created a class test user that will recognize the userclass.py

--- user_test.py
+++ user_test.py
@@ -2,4 +2,12 @@ import unittest
 
 from user import User
 
-class TestUser(unittest.TestCase)
+class TestUser(unittest.TestCase):
+
+    """
+    Test class that defines test cases for the user class behaviours
+
+    Arguments:
+    unittest.TestCase: TestCase class that helps in creating test cases
+    """
+

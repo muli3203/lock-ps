06a8eb8d357838e5c3fa39127dcd4935275637dd
muli3203 <muli3203@gmail.com>
2020-01-26 21:22:42 +0300

    created an instance of an object using the init method

--- credentials.py
+++ credentials.py
@@ -3,3 +3,9 @@ class Credential:
     this is a class that generates new credentials
     """
     credential_list = []
+
+def __init__(self,username,password, appname):
+    self.username
+    self.password
+    self.appname
+

0d1bf18efe0fae26f37f4162927120eac81be221
muli3203 <muli3203@gmail.com>
2020-01-26 12:50:34 +0300

    created a function that will call the user's  username and password

--- user.py
+++ user.py
@@ -2,3 +2,10 @@ class User:
     """
     Class that generates new instances of credentials
         """
+
+
+    def __init__(self, username, passworrd):
+        self.username = username
+        self.password = password
+
+    pass

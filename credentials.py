8edb8638c7e7058a4e1e863d1ef3d117039877fc
muli3203 <muli3203@gmail.com>
2020-01-27 08:25:09 +0300

    practised proper indentation

--- credentials.py
+++ credentials.py
@@ -4,19 +4,20 @@ class Credential:
     """
     credential_list = []
 
-def __init__(self, username, password, appname):
-    
-    """
-    __init__ method help us define properties for the objects 
-    """
-    self.username = username
-    self.password = password
-    self.appname = appname
+    def __init__(self, username, password, appname):
+        
+        """
+        __init__ method help us define properties for the objects 
+        """
+        self.username = username
+        self.password = password
+        self.appname = appname
 
+    @classmethod
+    def credential_exist(cls):pass
 
-
-def save_credential(self):
-    """
-    the save credential method saves the user objects into the credential list
-    """
-    Credential.credential_list.append(self)
+    def save_credential(self):
+        """
+        the save credential method saves the user objects into the credential list
+        """
+        Credential.credential_list.append(self)
